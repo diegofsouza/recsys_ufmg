d00d7a1af14c8b2c81b4ae59dbd68a656041499f
yc26df1 <dfurtado@avenuecode.com>
2014-09-24 19:19:55 -0300

    Initial commit

---
+++ src/main/java/br/ufmg/App.java
@@ -0,0 +1,13 @@
+package br.ufmg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
